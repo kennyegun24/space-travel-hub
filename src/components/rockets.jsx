9aec4828e401a969c79f2fb914b53cb5ced16acf
kenny Egun <kennyegun240@gmail.com>
2022-11-21 16:01:59 +0100

    Add format of rockets page

---
+++ src/components/rockets.jsx
@@ -0,0 +1,57 @@
+import Rocket from '../assets/tmg-facebook_social.jpg';
+
+const Rockets = () => (
+  <div className="rocket">
+    <div className="rocketDiv">
+      <div>
+        <img src={Rocket} className="rocketImg" alt="" />
+      </div>
+      <div className="rocketDivSm">
+        <h2>Falcon</h2>
+        <p>
+          I am a very handsome boy, LOL, I meant ugly, yeah, I am really ugly...
+          I love life and I am very optimistic about the future :)... Life is good
+        </p>
+        <button type="button" className="rocketBtn">Reserve Rocket</button>
+      </div>
+    </div>
+    <div className="rocketDiv">
+      <div>
+        <img src={Rocket} className="rocketImg" alt="" />
+      </div>
+      <div className="rocketDivSm">
+        <h2>Falcon</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque veniam
+          iure consectetur fugit! Quod voluptatibus porro
+          illum neque iure. Cupiditate et aliquid facere eius veniam rem
+          molestiae dolorem fugiat dolores?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo blanditiis explicabo
+          eaque animi tenetur eius a debitis ex alias iusto amet
+          quasi voluptas, obcaecati sequi non eligendi fugit aperiam odit.
+        </p>
+        <button type="button" className="rocketBtn">Reserve Rocket</button>
+      </div>
+    </div>
+    <div className="rocketDiv">
+      <div>
+        <img src={Rocket} className="rocketImg" alt="" />
+      </div>
+      <div className="rocketDivSm">
+        <h2>Falcon</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque veniam
+          iure consectetur fugit! Quod voluptatibus porro
+          illum neque iure. Cupiditate et aliquid facere eius veniam rem
+          molestiae dolorem fugiat dolores?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo blanditiis explicabo
+          eaque animi tenetur eius a debitis ex alias iusto amet
+          quasi voluptas, obcaecati sequi non eligendi fugit aperiam odit.
+        </p>
+        <button type="button" className="rocketBtn">Reserve Rocket</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default Rockets;
