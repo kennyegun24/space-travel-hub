befb3a391eea9593977a9efd7c6dc2a7ef10d115
kenny Egun <kennyegun240@gmail.com>
2022-11-21 16:00:33 +0100

    Add user profie to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,10 +3,15 @@ import logo from '../assets/saturn.png';
 
 const Navbar = () => (
   <div className="nav">
-    <img src={logo} alt="icon" className="logoimg" />
+    <div className="navFst">
+      <img src={logo} alt="icon" className="logoimg" />
+      <h1>Space Hub</h1>
+    </div>
     <div className="nav-sec">
       <Link className="Link" to="./">Rockets</Link>
       <Link className="Link" to="./missions">Missions</Link>
+      <p className="horizontal" />
+      <Link className="Link" to="./profile">User-Profile</Link>
     </div>
   </div>
 );
