3adfb324a83d99fb41c5a69cbbf913feb5165b7e
kenny Egun <kennyegun240@gmail.com>
2022-11-21 15:59:47 +0100

    Add rockets page and link to page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Rockets from './components/rockets';
 import Missions from './components/missions';
+import Profile from './components/userProfile';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Rockets />} />
         <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
