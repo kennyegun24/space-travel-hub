d584ee42971b72c6b016869e30f92a47fc5690d0
kenny Egun <kennyegun240@gmail.com>
2022-11-22 10:56:28 +0100

    Use slice method instead of actions, reducers and action creatord

--- src/Redux/rockets/rockets.js
+++ src/Redux/rockets/rockets.js
@@ -1,40 +1,42 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-// ACTIONS
-const FETCH_ASYNC_ROCKETS = 'space-travel-hub/rockets/GET_ROCKETS';
-const GET_ROCKETS = 'space-travel-hub/rockets/GET_ROCKETS/rejected';
 const BASE_ROCKET_URL = 'https://api.spacexdata.com/v3/rockets';
-// DEFAULT BOOKS
-
-const displayRockets = [];
+// DEFAULT
+const initialState = {
+  rockets: [],
+  status: null,
+};
 
 // ACTIONS CREATORS
-
 export const fetchRockets = createAsyncThunk(
-  FETCH_ASYNC_ROCKETS,
+  'fetch',
   async () => {
     const response = await fetch(BASE_ROCKET_URL);
     const data = await response.json();
-    const payload = data.map((eachRocket) => ({
-      id: eachRocket.rocket_id,
-      name: eachRocket.rocket_name,
-      type: eachRocket.rocket_type,
-      description: eachRocket.description,
-      images: eachRocket.flickr_images,
-    }));
-    return payload;
+    return data;
   },
 );
 
-// REDUCERS
-const rocketReducer = (state = displayRockets, action) => {
-  switch (action.type) {
-    case GET_ROCKETS: {
-      return action.payload;
-    }
-    default:
-      return state;
-  }
-};
+const rocketSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducer: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchRockets.pending, (state) => {
+        const isPending = state;
+        isPending.status = 'pending';
+      })
+      .addCase(fetchRockets.fulfilled, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.status = 'fulfilled';
+        isFulfilled.rockets = action.payload;
+      })
+      .addCase(fetchRockets.rejected, (state) => {
+        const isRejected = state;
+        isRejected.status = 'rejected';
+      });
+  },
+});
 
-export default rocketReducer;
+export default rocketSlice.reducer;
