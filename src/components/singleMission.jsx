8ce8b58d75b03e13678a17be1eb8ef53bb5511fa
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2022-11-24 09:50:47 +0200

    Add functions to handle mission's API calls
    Display data From API and add interactivity

--- src/components/singleMission.jsx
+++ src/components/singleMission.jsx
@@ -1,23 +1,47 @@
-// import { useSelector } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { toggleMember } from '../Redux/missions/missions';
 
-const singleMission = (mission) => {
-  const hello = () => 'hello';
+const SingleMission = ({
+  id, name, description, member,
+}) => {
+  const dispatch = useDispatch();
+
+  const handleClick = (missionId) => dispatch(toggleMember(missionId));
 
   return (
-    <tr className="mission-details">
-      <td>{mission.name}</td>
-      <td>{mission.description}</td>
+    <tr className="mission-details" id={id}>
+      <td>{name}</td>
+      <td>{description}</td>
       <td>
-        <div className="member-badge">Not a Member</div>
+        <div
+          className={`member-badge ${
+            member ? 'active-member' : 'not-active-member'
+          }`}
+        >
+          {member ? 'Active Member' : 'NOT A MEMBER'}
+        </div>
       </td>
       <td>
-        <button type="button" className="join-btn">
-          Join Mission
+        <button
+          type="button"
+          className={`mission-btn ${
+            member ? 'leave-action' : 'join-action'
+          }`}
+          onClick={() => handleClick(id)}
+        >
+          {member ? 'Leave Mission' : 'Join Mission'}
         </button>
       </td>
-      <div>{hello()}</div>
     </tr>
   );
 };
 
-export default singleMission;
+SingleMission.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  member: PropTypes.bool.isRequired,
+};
+
+export default SingleMission;
--- src/pages/missions.jsx
+++ src/pages/missions.jsx
@@ -1,15 +1,52 @@
-// import singleMission from '../components/singleMission';
+import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uniqueId } from 'uuid';
 
-const Missions = () => (
-  <section className="missions-section">
-    <table className="missions-table">
-      <tr>
-        <th>Mission</th>
-        <th>Description</th>
-        <th>Status</th>
-      </tr>
-    </table>
-  </section>
-);
+import { fetchMissions } from '../Redux/missions/missions';
+import SingleMission from '../components/singleMission';
 
+const Missions = () => {
+  const { missionsArray, loading } = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+  const shouldFetch = useRef(true);
+
+  useEffect(() => {
+    if (shouldFetch.current && loading === null) {
+      shouldFetch.current = false;
+      dispatch(fetchMissions());
+    }
+  }, []);
+
+  return (
+    <section className="missions-section">
+      {loading ? (
+        <div className="pending">
+          <p className="round" />
+          <p>LOADING...</p>
+        </div>
+      ) : (
+        <table className="missions-table">
+          <thead>
+            <tr>
+              <th>Mission</th>
+              <th>Description</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {missionsArray.map((element) => (
+              <SingleMission
+                key={uniqueId()}
+                id={element.id}
+                name={element.name}
+                description={element.description}
+                member={element.member}
+              />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </section>
+  );
+};
 export default Missions;
