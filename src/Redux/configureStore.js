30098247fe47af8739f2a02c022a7724bf288de7
BELINDA <kennyegun240@gmail.com>
2022-12-05 15:17:33 +0100

    Add dragn into the store

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,10 +2,12 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import rocketReducer from './rockets/rockets';
 import missionReducer from './missions/missions';
+import dragonReducer from './draons/dragons';
 
 const allReducers = combineReducers({
   rockets: rocketReducer,
   missions: missionReducer,
+  dragons: dragonReducer,
 });
 
 const store = configureStore({
